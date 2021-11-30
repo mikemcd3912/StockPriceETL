dba9abbf0bd20d29489cc091ec5027ae8d9b705c
Mike McDonald <mike.mcdonald3912@gmail.com>
2021-11-29 20:25:01 -0800

    Using Debugger to examine "history" object from yfinance

--- etl.py
+++ etl.py
@@ -3,7 +3,9 @@ import yfinance as yf
 import webbrowser
 import holdings_and_symbols as superInvestors
 
+msft = yf.Ticker("MSFT")
 
+hist = msft.history(period="max")
 
 if __name__ == '__main__':
     holdings, tickers = superInvestors.allCurrentHoldings()
